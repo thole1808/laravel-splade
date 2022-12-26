a2973cfd99588cfa1a5d55b2747e446edf7a3a1c
achmadagus <achmadagushadiyanto18@gmail.com>
2022-12-26 11:27:02 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::middleware('splade')->group(function () {
     Route::spladeUploads();
 
     Route::get('/', function () {
-        return view('welcome');
+        return view('auth.login');
     });
 
     Route::middleware('auth')->group(function () {

0b43366118aa3ee08c46c68e81066d4ce31b62ca
achmadagus <achmadagushadiyanto18@gmail.com>
2022-12-26 11:20:34 +0700

    update laravel splade with breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,13 +14,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['splade'])->group(function () {
-    Route::get('/', fn () => view('home'))->name('home');
-    Route::get('/docs', fn () => view('docs'))->name('docs');
-
+Route::middleware('splade')->group(function () {
     // Registers routes to support Table Bulk Actions and Exports...
     Route::spladeTable();
 
     // Registers routes to support async File Uploads with Filepond...
     Route::spladeUploads();
+
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::middleware('auth')->group(function () {
+        Route::get('/dashboard', function () {
+            return view('dashboard');
+        })->middleware(['verified'])->name('dashboard');
+
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
+
+    require __DIR__.'/auth.php';
 });
